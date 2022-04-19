3982669300dea71887138077c3d63a0ec058b436
Bence A. Tóth <tothab@gmail.com>
2022-04-19 14:46:52 +0200

    Basic store

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:3001/products")
+      .then((response) => response.json())
+      .then((result) => setProducts(result));
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {products.map((product) => (
+        <div key={product.id}>
+          <h2>{product.name}</h2>
+          {product.images.map((image, index) => (
+            <img
+              key={index}
+              src={`/images/products/${image}`}
+              style={{ height: "200px" }}
+              alt=""
+            />
+          ))}
+          <div>
+            <button
+              onClick={() => {
+                window.location.href = `http://localhost:3001/checkout?id=${product.id}`;
+              }}
+            >
+              Buy this
+            </button>
+          </div>
+        </div>
+      ))}
     </div>
   );
-}
+};
 
 export default App;
