dfb486fef8f78db541c685487f43c07371448ccd
Bence A. Tóth <tothab@gmail.com>
2022-04-19 15:55:00 +0200

    Variants

--- src/App.js
+++ src/App.js
@@ -3,10 +3,13 @@ import "./App.css";
 
 const App = () => {
   const [products, setProducts] = useState([]);
+
   useEffect(() => {
     fetch(`${process.env.REACT_APP_API_URL}/products`)
       .then((response) => response.json())
-      .then((result) => setProducts(result));
+      .then((result) =>
+        setProducts(result.map((p) => ({ ...p, selectedVariant: 0 })))
+      );
   }, []);
 
   return (
@@ -14,6 +17,7 @@ const App = () => {
       {products.map((product) => (
         <div key={product.id}>
           <h2>{product.name}</h2>
+          <p>{product.description}</p>
           {product.images.map((image, index) => (
             <img
               key={index}
@@ -23,9 +27,31 @@ const App = () => {
             />
           ))}
           <div>
+            <span>Variants:</span>
+            <select
+              onChange={(event) => {
+                setProducts((previousProducts) =>
+                  previousProducts.map((p) => {
+                    if (p.id === product.id) {
+                      return { ...p, selectedVariant: +event.target.value };
+                    }
+                    return p;
+                  })
+                );
+              }}
+            >
+              {product.variants.map((variant, index) => (
+                <option key={index} value={index}>
+                  {variant.name} ({variant.size})
+                </option>
+              ))}
+            </select>
+          </div>
+          <div>{product.variants[product.selectedVariant].price}</div>
+          <div>
             <button
               onClick={() => {
-                window.location.href = `${process.env.REACT_APP_API_URL}/checkout?id=${product.id}`;
+                window.location.href = `${process.env.REACT_APP_API_URL}/checkout?id=${product.id}&variant=${product.selectedVariant}`;
               }}
             >
               Buy this
